e083634d18d9e8b1182f8b97cc49915ccb6c9ac1
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-03-18 13:21:41 +0530

    Update faultycalc.py

--- faultycalc.py
+++ faultycalc.py
@@ -1,25 +1,21 @@
 print("Welcome!\n")
-print("Choose an operation:")
-print("1.Addition(+)")
-print("2.Subtraction(-)")
-print("3.Multiplication(*)")
-print("4.Division(/)")
-operator = input("-->")
+print("Select Operation:\n","1.Addition(+)\n","2.Subtraction(-)\n","3.Multiplication(*)\n","4.Division(/)\n")
+op = input("Operator -->")
 num1 = input("First Number:")
 num2 = input("Second Number:")
 print("Result:")
-result = num1 + operator + num2
+result = num1 + op + num2
 if result == "45*3":
     print(555)
 elif result == "56+9":
     print(77)
 elif result == "56/6":
     print(9)
-elif operator == "+":
+elif op == "+":
     print(int(num1) + int(num2))
-elif operator == "-":
+elif op == "-":
     print(int(num1) - int(num2))
-elif operator == "*":
+elif op == "*":
     print(int(num1) * int(num2))
-elif operator == "/":
+elif op == "/":
     print(int(num1) / int(num2))

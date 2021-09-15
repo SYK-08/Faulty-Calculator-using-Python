e85ae1d7b25e321f5ac4ee8298e1226219b5b826
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-09-15 18:49:43 +0530

    Make it bugfree.

--- faultycalc.py
+++ faultycalc.py
@@ -1,21 +1,48 @@
 print("Welcome!\n")
-print("Select Operation:\n","1.Addition(+)\n","2.Subtraction(-)\n","3.Multiplication(*)\n","4.Division(/)\n")
-op = input("Operator -->")
-num1 = input("First Number:")
-num2 = input("Second Number:")
-print("Result:")
-result = num1 + op + num2
-if result == "45*3":
-    print(555)
-elif result == "56+9":
-    print(77)
-elif result == "56/6":
-    print(9)
-elif op == "+":
-    print(int(num1) + int(num2))
-elif op == "-":
-    print(int(num1) - int(num2))
-elif op == "*":
-    print(int(num1) * int(num2))
-elif op == "/":
-    print(int(num1) / int(num2))
+
+def calculator():
+    print("Select Operation:\n","1.Addition(+)\n","2.Subtraction(-)\n","3.Multiplication(*)\n","4.Division(/)\n")
+
+    try:
+        op = input("Operator -->")
+        num1 = input("First Number:")
+        num2 = input("Second Number:")
+        print("Result:")
+        result = num1 + op + num2
+        
+        if result == "45*3":
+            print(555)
+        elif result == "56+9":
+            print(77)
+        elif result == "56/6":
+            print(9)
+        elif op == "+":
+            print(float(num1) + float(num2))
+        elif op == "-":
+            print(float(num1) - float(num2))
+        elif op == "*":
+            print(float(num1) * float(num2))
+        elif op == "/":
+            print(float(num1) / float(num2))
+        
+        else:
+            print("\n\t\tInvalid input! Please try again.\n")
+            calculator()
+    
+    except Exception as e:
+        print("\n\t\tSomething went wrong! Please try again.\n")
+        calculator()
+
+def run_again():
+    while True:
+        query = input("Do you want to calculate more?(y/n)").lower()
+        if query == "n":
+            exit()
+        elif query == "y":
+            calculator()
+        else:
+            print("\n\t\tInvalid input! Please try again.\n")
+            run_again()
+
+calculator()
+run_again()
